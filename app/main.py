452ff1746bca41935e4eb7a806509b614b3b7da2
mohodejimi <mohammedodejimi@gmail.com>
2026-09-22 12:38:45 +0100

    feat(fix): configure CORS for frontend requests

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMddleware
+
+origins = [
+    "http://localhost:3000"
+]
 
 from app.routes import router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMddleware, 
+    allow_origin = origins,
+    allow_credentials = False,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.include_router(router)
